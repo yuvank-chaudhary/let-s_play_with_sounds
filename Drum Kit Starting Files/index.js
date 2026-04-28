c3fbfa4b04eb51004e7e4f3fd41689174ec979dc
yuvank chaudhary <yuvankchoudhary@gmail.com>
2026-04-28 16:17:50 +0530

    third commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -18,12 +18,29 @@
 
 
 
+//  button pressed
+
 
 var totalNumberOfButton = document.querySelectorAll(".drum").length;
 for (var i = 0; i < totalNumberOfButton; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
+        makeSound(buttonInnerHTML)
+    });
+}
+
+// key pressed
+
+
+document.addEventListener("keypress", function (event){
+    makeSound(event.key)
+});
+
+
+function makeSound(key){
+
+
+     switch (key) {
             case "w":
                 var tom1 = new Audio('sounds/tom-1.mp3');
                 tom1.play();
@@ -64,8 +81,6 @@ for (var i = 0; i < totalNumberOfButton; i++) {
 
 
 
-  
-    });
 }
 
 
